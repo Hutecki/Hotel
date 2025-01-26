599617ed2e4f6ccee99a531d2d61242ded9bba24
Hutecki <hubert0706@wp.pl>
2025-01-26 13:45:31 +0100

    testing changes

--- app/page.jsx
+++ app/page.jsx
@@ -2,17 +2,26 @@ import React from "react";
 import SearchBar from "@/components/SearchBar";
 import { redirect } from "next/navigation";
 import { checkAuthentication } from "@/services/authenticate";
-import { setVrvMode, getVrvMode } from "@/services/vrvMode";
+import { getVrvMode } from "@/services/vrvMode";
+
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
 const HomePage = async () => {
   const isAuthenticated = await checkAuthentication();
 
   if (!isAuthenticated) {
+    await delay(1000);
     redirect("/login");
+    return null;
   }
+
   const vrvMode = await getVrvMode();
+
   if (vrvMode) {
     redirect("/aggregate");
+    return null;
   }
+
   return (
     <div className="flex flex-col justify-center items-center h-screen">
       <SearchBar />
